cccb5695262a78dbf1cf6a6eb61565cebc221341
HajeraSultana <e031306@gmail.com>
2024-08-31 22:08:26 +0100

    Create day8.py

---
+++ day8.py
@@ -0,0 +1,15 @@
+print("Custom Affirmation Generator")
+print()
+print("Hello! I'm Affirmation Generator and I'm here to help you to get positive vibes in your life!")
+print()
+name = input("Who are you? ")
+dow = input("What day of the week is it?")
+superpower = input("What is your superpower?")
+if dow == "Monday" or dow == "monday" or dow == "Tuesday" or dow == "tuesday":
+  print("Hello", name, "! Today is", dow, "and you are going to", superpower, "today! and remember every setback is an opportunity for growth and learning")
+elif dow == "Wednesday" or dow == "wednesday" or dow == "Thursday" or dow == "thursday":
+  print("Greetings", name, "! Start your ", dow, "by beliving in yourself and your abilities - you are amazing" )
+elif dow == "Friday" or dow == "friday":
+  print("Hey", name, "! start your", dow, "with a trust that today will bring you closer to your goals and dreams")
+else:
+  print("Hey", name, "! Welcome to weekend with an open heart, ready to relax and recharge")
